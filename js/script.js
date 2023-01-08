670be591008f4b6327a859dac5444c25af695c9e
Andrey Ilc <geleon2170@gmail.com>
2023-01-08 18:28:07 +0300

    1)create the variable numberOfFilms. 2)create the object personalMovieDB and added other fields. 3) ask a questions for user, and write questions in object personalMovieDB.movies

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numbberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numbberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько его оцените?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько его оцените?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
